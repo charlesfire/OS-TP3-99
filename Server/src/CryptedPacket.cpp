dd93d65d88d8c647fbc578eaeb5b2d5d58d0c434
Charlesfire <charlesfire2@hotmail.com>
2015-12-02 09:33:00 -0500

    Finalisation de l'algorithme de cryptage.

--- Server/src/CryptedPacket.cpp
+++ Server/src/CryptedPacket.cpp
@@ -2,13 +2,46 @@
 
 using namespace JC9;
 
-/*void CryptedPacket::onSend(std::size_t& size)
+const void* CryptedPacket::onSend(std::size_t& size)
 {
-    const void* srcData = getData();
-    std::size_t srcSize = getDataSize();
+    sf::Uint8* data = reinterpret_cast<sf::Uint8*>(const_cast<void*>(getData()));
+    for (unsigned int i(0); i < size; i++)
+    {
+        sf::Uint8 total = 0;
+        for (unsigned int j(0); j < 4; j++)
+        {
+            sf::Uint8 b1 = data[i] << (j * 2);
+            sf::Uint8 b2 = data[i] << (j * 2 + 1);
+            b1 = b1 >> 7;
+            b2 = b2 >> 7;
+
+            total = total | (b2 << ((3 - j) * 2 + 1)) | (b1 << ((3 - j) * 2));
+        }
+
+        data[i] = total;
+    }
+
+    return data;
 }
 
 void CryptedPacket::onReceive(const void* data, std::size_t size)
 {
+    sf::Uint8* decryptedData = reinterpret_cast<sf::Uint8*>(const_cast<void*>(data));
+    for (unsigned int i(0); i < size; i++)
+    {
+        sf::Uint8 total = 0;
+        for (unsigned int j(0); j < 4; j++)
+        {
+            sf::Uint8 b1 = decryptedData[i] << (j * 2);
+            sf::Uint8 b2 = decryptedData[i] << (j * 2 + 1);
+            b1 = b1 >> 7;
+            b2 = b2 >> 7;
 
-}*/
+            total = total | (b2 << ((3 - j) * 2 + 1)) | (b1 << ((3 - j) * 2));
+        }
+
+        decryptedData[i] = total;
+    }
+
+    append(decryptedData, size);
+}
